1053e47ef622929b327cc363228001d39613ae96
AdamCSmithCWS <adamcsmith.birdstats@gmail.com>
2023-11-13 13:07:32 -0500

    adding start-year modifications for selected species

--- bbs_script_doparallel.R
+++ bbs_script_doparallel.R
@@ -8,7 +8,7 @@ library(doParallel)
 
 setwd("C:/github/HRE_testing")
 
-machine = 1
+machine = 2
 n_cores = 4
 
 
@@ -44,17 +44,33 @@ test <- foreach(i = 1:9, #nrow(sp_list),
     aou <- as.integer(sp_list[i,"aou"])
 
 
+# identifying first years for selected species ----------------------------
+    fy <- NULL
+    if(aou %in% c(4661,4660)){ #Alder and Willow Flycatcher
+      fy <- 1978 #5 years after the split
+    }
+    if(aou %in% c(10,11,22860)){ # Clark's and Western Grebe and EUCD
+      fy <- 1990 #5 years after the split and first year EUCD observed on > 3 BBS routes
+    }
+    if(aou == 6121){ # CAve Swallow
+      fy = 1985
+    }
+
+
+
    strat <- "bbs_cws"
 
    s <- stratify(by = strat,
               species = sp,
               quiet = TRUE) %>%
   prepare_data(min_max_route_years = 2,
-               quiet = TRUE)
+               quiet = TRUE,
+               min_year = fy)
 
+   ## bbsBayes2 models do not currently work unless n_strata > 1
    if(nrow(s$meta_strata) == 1){stop(paste("Only 1 stratum for",sp,"skipping to next species"))}
 
-   if(nrow(s$meta_strata) > 3){
+   if(nrow(s$meta_strata) > 2){ #spatial models are irrelevant with < 3 strata
   bbs_dat <- prepare_spatial(s,
                   strata_map = load_map(strat)) %>%
   prepare_model(.,
@@ -68,7 +84,7 @@ test <- foreach(i = 1:9, #nrow(sp_list),
    }
 
 fit <- run_model(model_data = bbs_dat,
-                 #refresh = 100,
+                 refresh = 100,
                  #iter_warmup = 300,
                  #iter_sampling = 100,
                  output_dir = "output",
